cc46957a5b6ce10d6cae5308327b285a766f50c0
nguyenminh136 <nguyenminh13692@gmail.com>
2025-09-24 12:07:16 +0700

    update sidebar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
 import { inter, spaceGrotesk } from "./fonts";
 import { ThemeProvider } from "@/context/theme-context";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset
+} from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 // import { NavigationHeader } from "@/components/header";
 
@@ -14,16 +27,34 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
       <body>
-        {/* <NavigationHeader /> */}
         <ThemeProvider>
           <SidebarProvider>
             <AppSidebar />
-            <main>
-              <SidebarTrigger />
-              <div className="flex h-screen flex-col md:overflow-hidden">
-                <div className="flex-grow overflow-y-auto">{children}</div>
-              </div>
-            </main>
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem className="hidden md:block">
+                        <BreadcrumbLink href="#">
+                          Building Your Application
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem>
+                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+              </header>
+              {children}
+            </SidebarInset>
           </SidebarProvider>
         </ThemeProvider>
       </body>
