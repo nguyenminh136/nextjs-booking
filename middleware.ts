f72a8fb673cee7221584dd55cb6a30ae68aa2518
nguyenminh136 <nguyenminh13692@gmail.com>
2025-10-17 11:51:55 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!api/auth|login|public).*)"], // các route cần login
+  matcher: ["/((?!login|api/auth).*)"], // các route cần login
 };
