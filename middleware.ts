fef62626310b121c063476c762412c0326c62169
nguyenminh136 <nguyenminh13692@gmail.com>
2025-10-17 12:00:48 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,23 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login"
+  },
+  callbacks: {
+    authorized({ req, token }) {
+      console.log("🔑 Token:", token);
+      console.log("📄 Path:", req.nextUrl.pathname);
+      // Cho phép truy cập /login và /api/auth/* mà không cần token
+      if (req.nextUrl.pathname.startsWith("/login")) return true;
+      if (req.nextUrl.pathname.startsWith("/api/auth")) return true;
+
+      // Các route khác bắt buộc phải có token
+      return !!token;
+    }
+  }
+});
 
 export const config = {
-  matcher: ["/((?!login|api/auth).*)"], // các route cần login
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"]
 };
